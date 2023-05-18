ec0ffde52b9cb8e512e696639ffcb9bb22e1df3d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-18 19:01:43 -0400

    Make request use https

--- src/weatherInfoGetter.js
+++ src/weatherInfoGetter.js
@@ -1,7 +1,7 @@
 const weatherInfoGetter = (location) => {
   const requestWeather = async () => {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=fc5008ce75624eafa22190524231305&q=${location}`
+      `https://api.weatherapi.com/v1/current.json?key=fc5008ce75624eafa22190524231305&q=${location}`
     );
     if (response.ok) {
       const weatherInfoJson = await response.json();
